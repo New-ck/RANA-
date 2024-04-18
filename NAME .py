eaeb0d8031fd20a9c0d08a3325b0f37d70746934
New-ck <167387473+New-ck@users.noreply.github.com>
2024-04-19 01:04:32 +0545

    Add files via upload

---
+++ NAME .py
@@ -0,0 +1,11 @@
+# These are supported funding model platforms
+
+github: [botolmehedi, babaslab, teamvvirus]
+patreon: botolbaba
+open_collective: botolbaba
+ko_fi: botolbaba
+tidelift: npm/botolbabatool
+community_bridge: botol-baba-tool
+liberapay: botolbaba
+issuehunt: botolbaba
+otechie: botolbaba
